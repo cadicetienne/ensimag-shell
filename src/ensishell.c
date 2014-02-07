f46b9b4880f7490e45ab5b37c21cabb55d55deb7
Gael <ribesg@yahoo.fr>
2014-02-07 12:42:07 +0100

    Session 1

--- src/ensishell.c
+++ src/ensishell.c
@@ -5,6 +5,8 @@
  * Ce code est distribué sous la licence GPLv3+.     *
  *****************************************************/
 
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,8 +17,24 @@
 #error "Variante non défini !!"
 #endif
 
+/*
+ * Calcule la taille d'un tableau de chaines.
+ */
+static size_t sizeOfArray(char** array) {
+	if (array == NULL) {
+		return 0;
+	}
+
+	size_t count = 0;
+	while (array[count] != NULL) {
+		count++;
+	}
+
+	return count;
+}
+
 int main() {
-        printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
+	printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
 	while (1) {
 		struct cmdline *l;
@@ -25,30 +43,60 @@ int main() {
 
 		l = readcmd(prompt);
 
-		/* If input stream closed, normal termination */
+		// Si l'input est fermé, on clos le programme
 		if (!l) {
 			printf("exit\n");
 			exit(0);
 		}
 
 		if (l->err) {
-			/* Syntax error, read another command */
+			// Erreur de syntaxe, commande suivante
 			printf("error: %s\n", l->err);
 			continue;
 		}
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
+		if (l->in) {
+			printf("in: %s\n", l->in);
+		}
 
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
+		if (l->out) {
+			printf("out: %s\n", l->out);
+		}
+
+		if (l->bg) {
+			printf("background (&)\n");
+		}
+
+		// Affiche chaque commande
+		for (i = 0; l->seq[i] != 0; i++) {
+			char** cmd = l->seq[i];
 			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
+			for (j = 0; cmd[j] != 0; j++) {
+				printf("'%s' ", cmd[j]);
+			}
 			printf("\n");
 		}
+
+		printf("\n");
+
+		int res = fork();
+
+		if (res == -1) {
+			perror("Fork failed: ");
+			exit(-1);
+		} else if (res == 0) {
+			char* argc = l->seq[0][0];
+			int nbArgs = sizeOfArray(l->seq[0]) - 1;
+			char* argv[nbArgs];
+			for (i = 0; i < nbArgs; i++) {
+				argv[i] = l->seq[0][i + 1];
+			}
+			execvp(argc, argv);
+		} else {
+			waitpid((pid_t)res, NULL, 0);
+		}
+
+		printf("\n");
 	}
 }
+
